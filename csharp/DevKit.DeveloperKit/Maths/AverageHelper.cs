3c6c834d3c73a2558b9286cd3e0b29f3eba4f5f9
AluminiumTech <14107316+AluminiumTech@users.noreply.github.com>
2021-07-29 22:33:27 +0100

    Update AverageHelper.cs

---
+++ csharp/DevKit.DeveloperKit/Maths/AverageHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AluminiumTech.DevKit.DeveloperKit.Maths{
+    public class AverageHelper{
+
+        public double ExponentOf(double value, double power)
+        {
+           return Math.Pow(value, (1.0 / power));
+        }
+
+        /// <summary>
+        /// Calculate the geometric mean of a given set of numbers.
+        /// </summary>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public double CalculateGeometricMean(double[] values)
+        {
+            double sum = 1;
+            
+            for(int index = 0; index < values.Length; index++)
+            {
+                sum *= values[index];
+            }
+
+            return ExponentOf(sum, values.Length);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public double CalculateArithmeticMean(double[] values)
+        {
+            double sum = 0;
+
+            for (int index = 0; index < values.Length; index++)
+            {
+                sum += index;
+            }
+
+            return sum / values.Length;
+        }
+    }
+}
