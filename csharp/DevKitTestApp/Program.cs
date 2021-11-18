73465869659e7c3f953154a98c1f70cecefc5bb2
AluminiumTech <14107316+AluminiumTech@users.noreply.github.com>
2021-11-18 11:12:04 +0000

    add testing app

---
+++ csharp/DevKitTestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections;
+using AluminiumTech.DevKit.DeveloperKit;
+
+Console.WriteLine("Hello, World!");
+    
+HashMap<string, string> hashMap = new();
+
+Hashtable table = new();
+
+table["binger"] = "dumb";
+
+hashMap.ImportHashtable(table);
+
+Console.WriteLine();
