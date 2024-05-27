6f53c12470175c100b613fb0dffc565a67c04ad9
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-05-27 09:48:23 +0100

    use IEnumerable instead of ICollection

--- AlastairLundy.Extensions.System/Collections/FrequencyOf.cs
+++ AlastairLundy.Extensions.System/Collections/FrequencyOf.cs
@@ -27,22 +27,22 @@ using System.Collections.Generic;
 namespace AlastairLundy.Extensions.System.Collections
 {
     /// <summary>
-    /// A class to assist in counting the number of times an object or objects appear in a Collection.
+    /// A class to assist in counting the number of times an object or objects appear in an IEnumerable.
     /// </summary>
     public static class FrequencyOf
     {
         /// <summary>
-        ///  Caculates the number of times an object appears in an ICollection.
+        ///  Caculates the number of times an object appears in an IEnumerable.
         /// </summary>
-        /// <param name="collection">The ICollection to be searched.</param>
+        /// <param name="enumerable">The IEnumerable to be searched.</param>
         /// <param name="obj">The object to be searched for.</param>
         /// <typeparam name="T"></typeparam>
-        /// <returns>the number of times the object appears in the ICollection.</returns>
-        public static int Object<T>(ICollection<T> collection, T obj)
+        /// <returns>the number of times the object appears in the IEnumerable.</returns>
+        public static int Object<T>(IEnumerable<T> enumerable, T obj)
         {
             int frequency = 0;
 
-            foreach (T item in collection)
+            foreach (T item in enumerable)
             {
                 if (item.Equals(obj))
                 {
@@ -54,16 +54,16 @@ namespace AlastairLundy.Extensions.System.Collections
         }
         
         /// <summary>
-        /// Caculates the number of times objects appears in an ICollection.
+        /// Caculates the number of times objects appears in an IEnumerable.
         /// </summary>
-        /// <param name="collection">The ICollection to be searched.</param>
-        /// <typeparam name="T">The type of objects in the ICollection.</typeparam>
+        /// <param name="enumerable">The IEnumerable to be searched.</param>
+        /// <typeparam name="T">The type of objects in the IEnumerable.</typeparam>
         /// <returns></returns>
-        public static Dictionary<T, int> Objects<T>(ICollection<T> collection)
+        public static Dictionary<T, int> Objects<T>(IEnumerable<T> enumerable)
         {
             Dictionary<T, int> items = new Dictionary<T, int>();
 
-            foreach (T item in collection)
+            foreach (T item in enumerable)
             {
                 if (items.ContainsKey(item))
                 {
