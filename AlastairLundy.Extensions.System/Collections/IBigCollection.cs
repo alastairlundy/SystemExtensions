1a4e69fa38fdf23b496ab74e131f13fa0fecdbde
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-06-06 13:54:25 +0100

    add IBigCollection interface

---
+++ AlastairLundy.Extensions.System/Collections/IBigCollection.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+namespace AlastairLundy.Extensions.System.Collections
+{
+    // ReSharper disable once TypeParameterCanBeVariant
+    public interface IBigCollection<T> : IEnumerable
+    {
+        ulong Count { get; }
+
+        bool IsReadOnly { get; }
+
+        void Add(T item);
+        void Clear();
+        void Remove(T item);
+        
+        bool Contains(T item);
+    }
+}
