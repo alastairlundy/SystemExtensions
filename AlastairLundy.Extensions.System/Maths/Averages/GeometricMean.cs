e5f711b6519296f12e95934895312feb39400f22
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-06-08 19:36:23 +0100

    Update GeometricMean.cs

--- AlastairLundy.Extensions.System/Maths/Averages/GeometricMean.cs
+++ AlastairLundy.Extensions.System/Maths/Averages/GeometricMean.cs
@@ -25,7 +25,6 @@
 using System;
 using AlastairLundy.Extensions.System.DecimalExtensions;
 using AlastairLundy.Extensions.System.DoubleExtensions;
-using AlastairLundy.Extensions.System.FloatExtensions;
 
 namespace AlastairLundy.Extensions.System.Maths.Averages
 {
