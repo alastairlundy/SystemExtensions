76d275badf7301c6bace19289ed039628513a23d
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-01-31 19:48:28 +0000

    add initial SystemExtensions testing

---
+++ SystemExtensions.Tests/Strings/Contains/ContainsSpacesTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+using AlastairLundy.Extensions.System.Strings;
+
+using Bogus.DataSets;
+using Xunit.Abstractions;
+
+namespace SystemExtensions.Tests.Strings.Contains;
+
+public class ContainsSpacesTests
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+    private readonly Lorem _lorem = new();
+
+    public ContainsSpacesTests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [Fact]
+    public void UnspacedWordDetection()
+    {
+        string text = _lorem.Word().Replace(" ", string.Empty).Replace(' ', 'a');
+
+        _testOutputHelper.WriteLine($"Text: {text}");
+        _testOutputHelper.WriteLine($"Text2: {text.Split(' ').ToString()}");
+
+        bool actual = text.ContainsSpaceSeparatedSubStrings();
+        
+        Assert.False(actual);
+    }
+    
+    [Fact]
+    public void SpacedWordsDetection()
+    {
+        string text = string.Join(' ', _lorem.Words(Random.Shared.Next(2, 4)));
+        
+        bool actual = text.ContainsSpaceSeparatedSubStrings();
+        
+        Assert.True(actual);
+    }
+
+    [Fact]
+    public void WordWithEmptyStringDetection()
+    {
+        string text = _lorem.Word() + " ";
+        
+        bool actual = text.ContainsSpaceSeparatedSubStrings();
+        
+        Assert.True(actual);
+    }
+}
