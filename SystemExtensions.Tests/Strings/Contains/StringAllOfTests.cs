beca2d5a77c4d5bcf4afe2a4c760da84487fbc02
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-01-31 22:55:39 +0000

    add additional Contains tests

---
+++ SystemExtensions.Tests/Strings/Contains/StringAllOfTests.cs
@@ -0,0 +1,85 @@
+﻿/*
+        MIT License
+       
+       Copyright (c) 2025 Alastair Lundy
+       
+       Permission is hereby granted, free of charge, to any person obtaining a copy
+       of this software and associated documentation files (the "Software"), to deal
+       in the Software without restriction, including without limitation the rights
+       to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+       copies of the Software, and to permit persons to whom the Software is
+       furnished to do so, subject to the following conditions:
+       
+       The above copyright notice and this permission notice shall be included in all
+       copies or substantial portions of the Software.
+       
+       THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+       IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+       FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+       AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+       LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+       OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+       SOFTWARE.
+   */
+
+using System;
+using System.Linq;
+
+using AlastairLundy.Extensions.System.Strings;
+
+using Bogus.DataSets;
+
+namespace SystemExtensions.Tests.Strings.Contains;
+
+public class StringAllOfTests
+{
+    private Lorem _lorem = new Lorem();
+    
+    [Fact]
+    public void AllOfChars()
+    {
+        char[] chars = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+        string str = String.Join("", chars);
+
+        bool actual = str.ContainsAllOf(chars);
+        
+        Assert.True(actual);
+    }
+
+    [Fact]
+    public void NotAllOfChars()
+    {
+        char[] chars = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+        string str = chars.Take(3).ToString()!;
+
+        bool actual = str.ContainsAllOf(chars);
+        
+        Assert.False(actual);
+    }
+    
+    [Fact]
+    public void AllOfStrings()
+    {
+        string[] words = _lorem.Words(Random.Shared.Next(2, 4));
+
+        string text = String.Join(" ", words);
+        
+        bool actual = text.ContainsAllOf(words);
+        
+        Assert.True(actual);
+    }
+
+    [Fact]
+    public void NotAllOfStrings()
+    {
+        string[] words = _lorem.Words(Random.Shared.Next(3, 10));
+
+        string text = String.Join(" ", words.Take(2));
+        
+        bool actual = text.ContainsAllOf(words);
+        
+        Assert.False(actual);
+    }
+}
---
+++ SystemExtensions.Tests/Strings/Contains/StringAnyOfTests.cs
@@ -0,0 +1,89 @@
+﻿/*
+        MIT License
+       
+       Copyright (c) 2025 Alastair Lundy
+       
+       Permission is hereby granted, free of charge, to any person obtaining a copy
+       of this software and associated documentation files (the "Software"), to deal
+       in the Software without restriction, including without limitation the rights
+       to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+       copies of the Software, and to permit persons to whom the Software is
+       furnished to do so, subject to the following conditions:
+       
+       The above copyright notice and this permission notice shall be included in all
+       copies or substantial portions of the Software.
+       
+       THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+       IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+       FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+       AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+       LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+       OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+       SOFTWARE.
+   */
+
+using System;
+using System.Linq;
+
+using AlastairLundy.Extensions.System.Strings;
+
+using Bogus;
+using Bogus.DataSets;
+
+namespace SystemExtensions.Tests.Strings.Contains;
+
+public class StringAnyOfTests
+{
+    private Lorem _lorem = new();
+    
+    [Fact]
+    public void AnyOfChars()
+    {
+        char[] chars = Chars.UpperCase.Take(5).ToArray();
+        
+        Random.Shared.Shuffle(chars);
+        
+        string str = String.Join("", chars);
+
+        bool actual = str.ContainsAnyOf(Chars.UpperCase.ToCharArray());
+        
+        Assert.True(actual);
+    }
+
+    [Fact]
+    public void NotAnyOfChars()
+    {
+        string str = _lorem.Word();
+
+        bool actual = str.ContainsAnyOf(Chars.Numbers.ToCharArray());
+        
+        Assert.False(actual);
+    }
+
+    [Fact]
+    public void AnyOfStrings()
+    {
+        string[] words = _lorem.Words();
+        string text = _lorem.Word();
+        
+        text += words.First();
+
+        bool actual = text.ContainsAnyOf(words);
+        
+        Assert.True(actual);
+    }
+
+    [Fact]
+    public void NotAnyOfStrings()
+    {
+        string[] words = _lorem.Words().ToArray();
+        string text = words.First();
+        
+        words = words.Skip(1).ToArray();
+
+        bool actual = text.ContainsAnyOf(words);
+        
+        Assert.False(actual);
+    }
+
+}
