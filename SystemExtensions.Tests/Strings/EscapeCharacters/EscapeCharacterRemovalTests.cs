6bebf7cd4e6df2d1a1bfa1ef530d8f7fc3d2a0e7
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-02-03 19:24:53 +0000

    add more tests

---
+++ SystemExtensions.Tests/Strings/EscapeCharacters/EscapeCharacterRemovalTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using AlastairLundy.Extensions.System.Strings;
+using Bogus;
+
+namespace SystemExtensions.Tests.Strings.EscapeCharacters;
+
+public class EscapeCharacterRemovalTests
+{
+    [Fact]
+    public void ContainsEscapedCharactersTest()
+    {
+        string text = "Hello World\r\n";
+
+        bool actual = text.ContainsEscapeCharacters();
+        
+        Assert.True(actual);
+    }
+
+    [Fact]
+    public void DoesntContainEscapeCharactersTest()
+    {
+        char[] chars = Chars.UpperCase.ToCharArray();
+        Random.Shared.Shuffle(chars);
+        
+        chars = chars.Take(5).ToArray();
+        
+        string text = String.Join("", chars);
+
+        bool actual = text.ContainsEscapeCharacters();
+        
+        Assert.False(actual);
+    }
+    
+    [Fact]
+    public void SuccessfullyRemoveEscapeCharsTests()
+    {
+        string text = "Hello World\r\n";
+
+        string expected = "Hello World";
+        
+        string actual = text.RemoveEscapeCharacters();
+        
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ SystemExtensions.Tests/Strings/SpecialCharacters/SpecialCharacterDetectorTests.cs
@@ -0,0 +1,23 @@
+﻿
+using AlastairLundy.Extensions.System.Strings;
+using Bogus;
+
+namespace SystemExtensions.Tests.Strings.SpecialCharacters;
+
+public class SpecialCharacterDetectorTests
+{
+    public static char[] lowerCaseChars = Chars.LowerCase.ToCharArray();
+    
+    [Theory]
+    public void NotASpecialCharacter(char character)
+    {
+        bool actual = character.IsSpecialCharacter();
+        
+        Assert.False(actual);
+    }
+
+    public void IsASpecialCharacter()
+    {
+        
+    }
+}
